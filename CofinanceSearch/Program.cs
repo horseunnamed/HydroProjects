2e02dc8e6f15af9a5b80edaae26e6f5938acd3a2
Steve <stevehonor@outlook.com>
2019-04-23 14:48:58 +0400

    Implement statistics aggregation

--- CofinanceSearch/Program.cs
+++ CofinanceSearch/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using CofinanceSearch.Stats;
 using Core;
 using Core.Channels;
@@ -12,22 +11,31 @@ namespace CofinanceSearch
     {
         static void Main(string[] args)
         {
-            // reading of grd maps from file:
-            var gridMap = GrdInteraction.ReadGridMapFromGrd(Dir.Data("something.grd"));
 
             // reading of channels tree from file:
             var channelsTree = CgInteraction.ReadChannelsTreeFromCg(Dir.Data("channels.cg"));
 
             // calculating stats:
+            var floodMap = GrdInteraction.ReadGridMapFromGrd(Dir.Data("only_territory_flood_frequency0.grd"));
+            var typeUseMap = GrdInteraction.ReadGridMapFromGrd(Dir.Data("TypeUseWithNonCadastr.grd"));
             var hozValues = new HashSet<double> { 37, 38, 39, 43, 44, 45 };
             var socValues = new HashSet<double> { 58, 61, 63, 66, 171, 186, 195, 203, 207 };
-            var stats = StatsAggregation.GetStats(channelsTree, gridMap, gridMap, hozValues, socValues);
+            var nonCadastrId = 8888d;
+            var r = 10;
+            var stats = StatsAggregation.GetStats(channelsTree, floodMap, typeUseMap, hozValues, socValues, nonCadastrId, r);
 
-            // usage of donors-acceptors algo
+
+            /* Usage of donors-acceptors algorithm */
             
+            // read flood series for Q=23 and t=[20, 39]:
+            var floodSeries = GrdInteraction.ReadFloodSeriesFromZip(Dir.Data("flood/23.zip"), 20, 39);
+
+            // read eco targets map
+            var ecoTargetMap = GrdInteraction.ReadGridMapFromGrd(Dir.Data("frequencies/add_frequency_from_0,65_to_0,85.grd"));
+
             // initialization of algo:
-            var donorsAcceptors = new DonorsAcceptors(DonorsAcceptors.RatingStrategy.TargetCount, 
-                channelsTree, gridMap, gridMap);
+            var donorsAcceptors = new DonorsAcceptors(
+                DonorsAcceptors.RatingStrategy.TargetCount, channelsTree, ecoTargetMap, floodSeries);
 
             // on each iteration call method Run with CofinanceInfo:
             var cofinanceInfo = new CofinanceInfo(0, null);
--- CofinanceSearch/Stats/StatsAggregation.cs
+++ CofinanceSearch/Stats/StatsAggregation.cs
@@ -1,15 +1,118 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Core.Channels;
 using Core.Grid;
 
 namespace CofinanceSearch.Stats
 {
-    class StatsAggregation
+    public class StatsAggregation
     {
-        public static ChannelSystemStats GetStats(ChannelsTree channelsTree, 
-            GridMap floodMap, GridMap typeUseMap, ISet<double> hozValues, ISet<double> socValues)
+        [SuppressMessage("ReSharper", "CompareOfFloatsByEqualityOperator")]
+        public static ChannelSystemStats GetStats(
+            ChannelsTree channelsTree, GridMap floodMap, GridMap typeUseMap, 
+            ISet<double> hozValues, ISet<double> socValues, double nonCadastrId, int r)
         {
-            return null;
+            var selfStatsDict = new Dictionary<Channel, Stats>();
+            var aggrStatsDict = new Dictionary<Channel, Stats>();
+
+            channelsTree.VisitChannelsFromTop(channel =>
+            {
+                var nonCadastrFlooded = 0;
+                var nonCadastrNotFlooded = 0;
+                var hozFlooded = 0;
+                var hozNotFlooded = 0;
+                var socNotFlooded = 0;
+
+                var visitedCells = new HashSet<(int, int)>();
+                foreach (var point in channel.Points)
+                {
+                    for (var x = point.X - r; x <= point.X + r; x++)
+                    {
+                        for (var y = point.Y - r; y <= point.Y + r; y++)
+                        {
+                            if (x >= 0 && x < typeUseMap.Width && y >= 0 && y < typeUseMap.Height && !visitedCells.Contains((x, y)))
+                            {
+                                var targetVal = typeUseMap[x, y];
+                                var floodVal = floodMap[x, y];
+                                if (targetVal == nonCadastrId)
+                                {
+                                    if (floodVal == 0)
+                                    {
+                                        nonCadastrNotFlooded++;
+                                    }
+                                    else
+                                    {
+                                        nonCadastrFlooded++;
+                                    }
+                                }
+                                else if (hozValues.Contains(targetVal))
+                                {
+                                    if (floodVal == 0)
+                                    {
+                                        hozNotFlooded++;
+                                    }
+                                    else
+                                    {
+                                        hozFlooded++;
+                                    }
+                                }
+                                else if (socValues.Contains(targetVal) && floodVal == 0)
+                                {
+                                    socNotFlooded++;
+                                }
+                            }
+                            visitedCells.Add((x, y));
+                        }
+                    }
+                }
+                selfStatsDict[channel] = new Stats(
+                   channel.Points.Count,
+                   nonCadastrFlooded,
+                   nonCadastrNotFlooded,
+                   hozFlooded,
+                   hozNotFlooded,
+                   socNotFlooded);
+            });
+
+            var channelsStats = new List<ChannelStats>();
+
+            channelsTree.VisitChannelsFromBottom(channel =>
+            {
+                var channelStats = selfStatsDict[channel];
+
+                var aggrLength = channelStats.Length;
+                var aggrNonCadastrFlooded = channelStats.NonCadastrFlooded;
+                var aggrNonCadastrNotFlooded = channelStats.NonCadastrNotFlooded;
+                var aggrHozFlooded = channelStats.HozFlooded;
+                var aggrHozNotFlooded = channelStats.HozNotFlooded;
+                var aggrSocNotFlooded = channelStats.SocNotFlooded;
+
+                foreach (var child in channel.Children)
+                {
+                    var childAggrStats = selfStatsDict[child];
+                    aggrLength += childAggrStats.Length;
+                    aggrNonCadastrFlooded += childAggrStats.NonCadastrFlooded;
+                    aggrNonCadastrNotFlooded += childAggrStats.NonCadastrNotFlooded;
+                    aggrHozFlooded += childAggrStats.HozFlooded;
+                    aggrHozNotFlooded += childAggrStats.HozNotFlooded;
+                    aggrSocNotFlooded += childAggrStats.SocNotFlooded;
+                }
+
+                var aggrStats = new Stats(
+                    aggrLength, 
+                    aggrNonCadastrFlooded, 
+                    aggrNonCadastrNotFlooded, 
+                    aggrHozFlooded, 
+                    aggrHozNotFlooded, 
+                    aggrSocNotFlooded);
+
+                aggrStatsDict[channel] = aggrStats;
+
+                channelsStats.Add(new ChannelStats(channel.Id, channelStats, aggrStats));
+            });
+
+
+            return new ChannelSystemStats(channelsStats);
         }
     }
 }
